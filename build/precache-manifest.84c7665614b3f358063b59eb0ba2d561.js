29402d7b8b6ea63cc99b1ee5a2a1d37f2689b4c3
Max <maxguojiaqi@gmail.com>
2020-07-04 00:32:45 -0400

    make a github build

---
+++ build/precache-manifest.84c7665614b3f358063b59eb0ba2d561.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ed8883ee7c8faf127c6d9a53aeeab4",
+    "url": "/Maxguojiaqi/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ecb80e8ddef07f9ba53a",
+    "url": "/Maxguojiaqi/React-Portfolio/static/css/main.0f055532.chunk.css"
+  },
+  {
+    "revision": "4a5020a72b514f8f2bfc",
+    "url": "/Maxguojiaqi/React-Portfolio/static/js/2.e8b9165c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Maxguojiaqi/React-Portfolio/static/js/2.e8b9165c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb80e8ddef07f9ba53a",
+    "url": "/Maxguojiaqi/React-Portfolio/static/js/main.400b792a.chunk.js"
+  },
+  {
+    "revision": "8ba298a46570e1ff3c29",
+    "url": "/Maxguojiaqi/React-Portfolio/static/js/runtime-main.44e6cbb8.js"
+  }
+]);
